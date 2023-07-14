2b2966d892ea5d8167446d3eec21f73003681a82
James Wenzel <jameswenzel@users.noreply.github.com>
2023-07-14 15:58:09 -0700

    fork w1nt3r's svg lib

---
+++ src/onchain/svg.sol
@@ -0,0 +1,103 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import {LibString} from "solady/utils/LibString.sol";
+
+/**
+ * @title Core SVG utilitiy library which helps us construct SVG's with a simple, web-like API.
+ * @author w1nt3r.eth
+ * @notice Modified from w1nt3r.eth's hot-chain-svg library: https://github.com/w1nt3r-eth/hot-chain-svg
+ */
+library svg {
+    using LibString for uint256;
+
+    function g(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("g", _props, _children);
+    }
+
+    function path(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("path", _props, _children);
+    }
+
+    function text(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("text", _props, _children);
+    }
+
+    function line(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("line", _props, _children);
+    }
+
+    function circle(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("circle", _props, _children);
+    }
+
+    function circle(string memory _props) internal pure returns (string memory) {
+        return el("circle", _props);
+    }
+
+    function rect(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("rect", _props, _children);
+    }
+
+    function rect(string memory _props) internal pure returns (string memory) {
+        return el("rect", _props);
+    }
+
+    function filter(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("filter", _props, _children);
+    }
+
+    function cdata(string memory _content) internal pure returns (string memory) {
+        return string.concat("<![CDATA[", _content, "]]>");
+    }
+
+    /* GRADIENTS */
+    function radialGradient(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("radialGradient", _props, _children);
+    }
+
+    function linearGradient(string memory _props, string memory _children) internal pure returns (string memory) {
+        return el("linearGradient", _props, _children);
+    }
+
+    function gradientStop(uint256 offset, string memory stopColor, string memory _props)
+        internal
+        pure
+        returns (string memory)
+    {
+        return el(
+            "stop",
+            string.concat(
+                prop("stop-color", stopColor), " ", prop("offset", string.concat(offset.toString(), "%")), " ", _props
+            )
+        );
+    }
+
+    function animateTransform(string memory _props) internal pure returns (string memory) {
+        return el("animateTransform", _props);
+    }
+
+    function image(string memory _href, string memory _props) internal pure returns (string memory) {
+        return el("image", string.concat(prop("href", _href), " ", _props));
+    }
+
+    /* COMMON */
+    // A generic element, can be used to construct any SVG (or HTML) element
+    function el(string memory _tag, string memory _props, string memory _children)
+        internal
+        pure
+        returns (string memory)
+    {
+        return string.concat("<", _tag, " ", _props, ">", _children, "</", _tag, ">");
+    }
+
+    // A generic element, can be used to construct any SVG (or HTML) element without children
+    function el(string memory _tag, string memory _props) internal pure returns (string memory) {
+        return string.concat("<", _tag, " ", _props, "/>");
+    }
+
+    // an SVG attribute
+    function prop(string memory _key, string memory _val) internal pure returns (string memory) {
+        return string.concat(_key, "=", '"', _val, '" ');
+    }
+}
