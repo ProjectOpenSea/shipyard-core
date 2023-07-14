34b83cddc27e7c2a1f966c558a1f8638c0ef3031
James Wenzel <jameswenzel@users.noreply.github.com>
2023-07-14 14:17:42 -0700

    add queryable interface

--- src/interfaces/IERC0002.sol
+++ src/interfaces/IERC0002.sol
@@ -6,5 +6,7 @@ pragma solidity ^0.8.17;
  * @notice This ERC proposes a standard interface for querying arbitrary storage slots in a contract.
  */
 interface IERC0002 {
-    function load(uint256 slot) external view returns (bytes32 value);
+    function extsload(bytes32 slot) external view returns (bytes32 value);
+
+    function extsload(bytes32[] calldata slots) external view returns (bytes32[] memory values);
 }
--- src/reference/ERC0002.sol
+++ src/reference/ERC0002.sol
@@ -4,13 +4,55 @@ pragma solidity ^0.8.17;
 import {IERC0002} from "src/interfaces/IERC0002.sol";
 
 /**
- * @title ERC0000 Queryable Reference Implementation
+ * @title ERC0002 Queryable Reference Implementation
  */
-
 contract ERC0002 is IERC0002 {
-    function load(uint256 slot) external view returns (bytes32 value) {
-        assembly {
-            value := sload(slot)
+    function extsload(bytes32 slot) external view returns (bytes32) {
+        assembly ("memory-safe") {
+            mstore(0, sload(slot))
+            return(0, 0x20)
+        }
+    }
+
+    function extsload(bytes32[] calldata slots) external view returns (bytes32[] memory) {
+        assembly ("memory-safe") {
+            // get free memory pointer; this is the pointer to the abi-encoded offset of the return array
+            let ptr := mload(0x40)
+            // store the abi-encoded offset of the return array
+            mstore(ptr, 0x20)
+            // assign values to the word after the offset
+            let values := add(0x20, ptr)
+
+            // store the length at 'values'
+            mstore(values, slots.length)
+            // location in memory to start writing values (one word after length)
+            let arrayStart := add(values, 0x20)
+
+            // calculate length of array in bytes
+            let slotsBytesLength := shl(5, slots.length)
+
+            // loop over slots and write sloads to memory
+            for { let idx }
+            // while idx < slots.bytes_length
+            lt(idx, slotsBytesLength) {
+                // after each loop, increment idx by one word
+                idx := add(0x20, idx)
+            } {
+                mstore(
+                    // store value at start + idx
+                    add(arrayStart, idx),
+                    // sload from slot
+                    sload(
+                        // load slot from calldata
+                        calldataload(
+                            // add idx bytes to slots.offset
+                            add(idx, slots.offset)
+                        )
+                    )
+                )
+            }
+            // return 1 word for the offset, 1 word for the length of the array, and the array itself
+            return(ptr, add(0x40, slotsBytesLength))
         }
     }
 }
